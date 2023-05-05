61a503b14ec14d3514a5b931b7f05e9ae74454aa
AlineNunes2023 <alinenunespp@gmail.com>
2023-05-04 21:07:38 -0300

    questão 1 e 2 - feita

---
+++ src/main.js
@@ -0,0 +1,14 @@
+console.log("teste")
+
+const numero = prompt("Digite um número")
+
+const questao1 = document.querySelector("#questao1")
+questao1.innerHTML = numero * 2
+
+let conta = parseInt(prompt("Qual o valor da conta?"))
+let resultado = conta + (conta*0.10)
+
+const questao2 = document.querySelector("#questao2")
+questao2.innerHTML = resultado
+
+
