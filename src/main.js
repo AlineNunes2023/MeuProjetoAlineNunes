d95c296f3e146aa7f752d20db3a6535822c87b9f
AlineNunes2023 <alinenunespp@gmail.com>
2023-05-05 21:31:19 -0300

    exercicios feitos

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
-console.log("teste")
-
-const numero = prompt("Digite um número")
+const numero = prompt("Digite um número");
 
 const questao1 = document.querySelector("#questao1")
 questao1.innerHTML = numero * 2
 
-let conta = parseInt(prompt("Qual o valor da conta?"))
+alert(numero)
+
+let conta = parseInt(prompt("Qual o valor da conta?"));
 let resultado = conta + (conta*0.10)
 
 const questao2 = document.querySelector("#questao2")
 questao2.innerHTML = resultado
 
+alert(conta)
 
+const conta2 = parseInt(prompt("Qual o valor total da conta?"));
+const conta3 = parseInt(prompt("Qual a quantidade de clientes?"));
+const resultado2 = conta2 / conta3
 
+const questao3 = document.querySelector("#questao3")
+questao3.innerHTML = resultado2 
+
+alert(conta2)
+alert(conta3)
